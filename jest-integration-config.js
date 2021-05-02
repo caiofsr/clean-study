133ea8d4f5fdbf788fc91c7c5099f9d3853e8fc0
Caio Fernando <caiofsr13@gmail.com>
2021-05-01 21:04:18 -0300

    chore: create custom configs to unit and integration tests

---
+++ jest-integration-config.js
@@ -0,0 +1,5 @@
+const config = require('./jest.config.js')
+
+config.testMatch = ['**/*.test.js']
+
+module.exports = config
---
+++ jest-unit-config.js
@@ -0,0 +1,5 @@
+const config = require('./jest.config.js')
+
+config.testMatch = ['**/*.spec.js']
+
+module.exports = config
